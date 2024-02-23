29723d6e29780a4ea29db9af7950595558e5d445
jbeden1 <160893914+jbeden1@users.noreply.github.com>
2024-02-23 16:58:49 +0100

    zad3

--- V1/zadatak3.py
+++ V1/zadatak3.py
@@ -1,7 +1,17 @@
 def unos():
-    x=int(input('Unesi x: '))
-    y=int(input('Unesi y: '))
+    try:
+         x=int(input('Unesi x: '))
+         y=int(input('Unesi y: '))
+    except:
+        print('Pogrtešan unos')
     return x, y
-unos()
-unos()
-
+a=unos()
+b=unos()
+def jednadžba_pravca(t1, t2):
+    if t1==t2:
+        print('Ima beskonačno pravaca')
+    elif t1[0]==t2[0] or t1[1]==t2[1]:
+        print('pravac je vertikalan ili vodoravan')
+    
+    
+    
