fc8646ad8725c1d9f540f530d28b641aca7b6f57
jbeden1 <160893914+jbeden1@users.noreply.github.com>
2024-04-13 00:06:32 +0200

    v5

--- V5/z1.py
+++ V5/z1.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 def fja(x):
    f = 5*x**3-2*x**2+2*x+3
    return f
+tocke = np.arange(-2, 2, 0.1)
+analiticka_derivacija = 5*3*tocke**2-2*2*tocke+2
 
 eps1 = calc.der_raspon(fja, -2, 2, h = 0.01)
 plt.plot(eps1[0], eps1[1], label = "eps = 0.01")
+plt.plot(tocke, analiticka_derivacija, label = "analitičko rješenje")
 plt.title("Derivacija")
 plt.xlabel("x")
 plt.ylabel("f´(x)")
@@ -16,15 +19,6 @@ plt.show()
 
 eps2 = calc.der_raspon(fja, -2, 2, h = 0.1)
 plt.plot(eps2[0], eps2[1], label = "eps = 0.1")
-plt.title("Derivacija")
-plt.xlabel("x")
-plt.ylabel("f´(x)")
-plt.legend()
-plt.show()
-
-tocke = np.arange(-2, 2, 0.1)
-analiticka_derivacija = 5*3*tocke**2-2*2*tocke+2
-
 plt.plot(tocke, analiticka_derivacija, label = "analitičko rješenje")
 plt.title("Derivacija")
 plt.xlabel("x")
